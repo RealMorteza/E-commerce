a5eab13393d3ca00359d2d61e265c07f82d41737
RealMorteza <162283324+RealMorteza@users.noreply.github.com>
2025-07-18 22:48:34 +0330

    Initial commit

---
+++ frontend/src/components/Assets/Products.js
@@ -0,0 +1,30 @@
+import p1_img from "./Products-images/Black-Tshirt.png";
+import p2_img from "./Products-images/Blue-Tshirt.png";
+import p3_img from "./Products-images/White-Tshirt.png";
+
+
+
+let Products = [
+    {
+        id: 1,
+        name: "تیشرت مشکی",
+        category: "مردانه",
+        image: p1_img,
+        price: "400"
+    },
+    {
+        id: 2,
+        name: "تیشرت آبی",
+        image: p2_img,
+        category: "مردانه",
+        price: "500"
+    },
+    {
+        id: 3,
+        name: "تیشرت سفید",
+        image: p3_img,
+        category: "مردانه",
+        price: "500"
+    }
+
+]
---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Navbar.css';
+import logo from '../Assets/General-images/main-logo.png';
+import cart_icon from '../Assets/General-images/cart-icon.png';
+
+
+const Navbar = () => {
+    return (
+        <div className='Navbar'>
+
+        </div>
+
+    )
+
+}
+export default Navbar 
